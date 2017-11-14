1d41c935585ada9e2d08b94c95c9918e446ba98d
tsaik9520 <31925310+tsaik9520@users.noreply.github.com>
2017-11-14 10:48:16 -0500

    Add files via upload

---
+++ Craps Lab/src/Die.java
@@ -0,0 +1,12 @@
+
+public class Die {
+    private int numDots;
+
+    public void roll() {
+        this.numDots = (int)(6.0 * Math.random()) + 1;
+    }
+
+    public int getNumDots() {
+        return this.numDots;
+    }
+}
